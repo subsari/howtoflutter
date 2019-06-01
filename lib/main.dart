1b30f55049d7bd58b2756c7efa2b25d71bccfc4a
Joel Pagan <subsari@gmail.com>
2019-06-01 14:02:28 -0500

    Add Basic Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -67,10 +67,9 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      drawer: Drawer(),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
